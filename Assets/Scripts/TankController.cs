b10874bff90c5bff1b3e73019ff58dfa7c108446
Chetan Rautela <chetanrautela20@gmail.com>
2024-04-22 11:51:17 +0530

    added rigidbody to tank

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -7,10 +7,9 @@ public class TankController
     private Tankmodel tankModel;
 
     public TankController(TankView tankView,Tankmodel tankModel)
-    {
-        this.tankView = tankView;
+    {       
         this.tankModel = tankModel;
-        GameObject.Instantiate(tankView.gameObject);
+        this.tankView = GameObject.Instantiate<TankView>(tankView);
         tankModel.SetTankController(this);
         tankView.SetTankController(this);
     }
